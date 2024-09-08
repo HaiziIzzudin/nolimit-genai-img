b8a317090019ded2ea517d08a9dddfb59602d663
haizi <thehub@iziizz.slmail.me>
2024-09-09 04:09:05 +0800

    hotfix

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/HaiziIzzudin/nolimit-genai-img.git;
-cd nolimit-genai-img;
-docker build -t myimage .;
+git clone https://github.com/HaiziIzzudin/nolimit-genai-img.git && \
+cd nolimit-genai-img && \
+docker build -t myimage . && \
 docker run -d --name mycontainer -p 80:80 myimage;
