05dc80423eb92ad3507678a797ca7aa83cd64322
haizi <thehub@iziizz.slmail.me>
2024-09-10 04:54:01 +0800

    d

--- setup.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/HaiziIzzudin/nolimit-genai-img.git && \
-cd nolimit-genai-img && \
-nano app/config.toml && \
-docker build -t myimage . && \
-docker run -d --name mycontainer -p 81:81 myimage
