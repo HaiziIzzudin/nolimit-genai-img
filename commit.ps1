273a3167ec81527cd8a2df106a1fec0b942fa763
haizi <thehub@iziizz.slmail.me>
2024-09-11 04:40:20 +0800

    2024-09-11 04:40:20

--- commit.ps1
+++ commit.ps1
@@ -1,7 +1,4 @@
 git add .
 git commit -m "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")"
 git push
-$remoteCommand = "cd /home/haizi/nolimit-gen-ai-images/ && git pull"
-$securePassword = ConvertTo-SecureString "Haizzudin02" -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential ("haizi", $securePassword)
-Invoke-Command -ComputerName "139.162.55.58" -Credential $cred -ScriptBlock { param($remoteCommand) iex $remoteCommand } -ArgumentList $remoteCommand
+ssh haizi@139.162.55.58 "cd nolimit-genai-img/ && git pull"
