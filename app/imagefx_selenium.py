ffc12c134bfac4d4c2e34bd28580b2033f7d17b9
haizi <thehub@iziizz.slmail.me>
2024-09-11 22:03:53 +0800

    2024-09-11 22:03:53

--- app/imagefx_selenium.py
+++ app/imagefx_selenium.py
@@ -36,6 +36,8 @@ def xpath(count:int=1):
     "antibot-btn": f"/html/body/div/div/div/div/form/button",
     "home-imagefx-btn": f"/html/body/div/div/div/div[2]/div[2]/div/div[3]/a",
 
+    "google-signin-btn": f"/html/body/div/div/div/div/div[2]/div[2]/div/span/button",
+
     "image-dlbtn": f"/html/body/div/div/div/div/div[1]/div[1]/div/div[{count}]/div/div[1]/div[1]/div[2]/div/button[2]",
               "err": f"/html/body/div/div/div/div/div[1]/div[1]/div/div[{count}]/h3",
     "textbox-prompt": "/html/body/div/div/div/div/div[1]/div[2]/div[2]/div[1]/div/div/div[1]/div[1]",
@@ -70,7 +72,7 @@ def profile_launch_and_login(headless:bool=False):
 
   # Navigate to a URL, resize window
   driver = webdriver.Firefox(options=options)
-  driver.set_window_size(1200, 900)
+  driver.set_window_size(1600, 1200)
   # driver.get(f"https://aitestkitchen.withgoogle.com/tools/image-fx")
   driver.get(f"https://aitestkitchen.withgoogle.com")
 
@@ -112,12 +114,21 @@ class main():
       print(YELLOW,'antibot button detected and clicked',RESET)
     except:
       print(GREEN,'No antibot button detected. Proceeding...',RESET)
-
-    # main logic
-    try:
       element = get_element(driver, xpath()['home-imagefx-btn'])
       element.click()
       print(GREEN,'<a> imagefx button clicked',RESET)
+
+    # test google sign in blocking
+    try:
+      element = get_element(driver, xpath()['google-signin-btn'])
+      print(RED,'Sign in button found! Exiting...',RESET)
+      exit(2)
+    except Exception as e:
+      print(GREEN,'Sign in button not found. Proceeding...',RESET)
+
+
+    # main logic
+    try:
       # insert prompt
       element = get_element(driver, xpath()['textbox-prompt'])
       actions.move_to_element(element).click().perform()
@@ -181,7 +192,7 @@ def return_for_api(prompt:str):  # for fastapi
 
 if __name__ == "__main__":
   mainprogram = main()
-  mainprogram.init_driver(headless=False)
+  mainprogram.init_driver(headless=True)
   mainprogram.run(cf['prompt'])
   
   # invoke opening folder if true
