6db736e38135c45e2f7a9cea16bdf25ec50c440c
Haizi <haizukun@iziizz.com>
2025-01-14 00:19:26 +0800

    feat: implement async image generation and return base64 data in response

--- app/main.py
+++ app/main.py
@@ -72,6 +72,28 @@ async def generate(prompt_request: PromptRequest):
   
   prompt = prompt_request.prompt
   print(MAGENTA,"Prompt:",prompt,RESET)
+  
+  # data = hf_api_fastapi(prompt)
+  task1 = asyncio.to_thread(hf_gradio_api, prompt) # which token is determined inside hf_token_api
+  data1 = await asyncio.gather(task1) # return base64 data
+
+  image_base64:list[str] = [data1]
+  total = len(image_base64)
+  print(MAGENTA,"total:",total,RESET)
+  
+  return Response(
+    content=json.dumps(
+      {
+        "image_base64": image_base64,  # you already returned base64 list
+        "total": total
+      }), 
+    headers={
+      "Access-Control-Allow-Origin": "https://imagen.ai.iziizz.com, https://iziizz-tasker.x10.mx/imagen-ai",
+      "Access-Control-Allow-Methods": "POST",
+      "Access-Control-Allow-Headers": "Content-Type",
+    }, 
+    media_type="application/json", 
+  )
 
 # maknanya logic utk nk display gambar 1 ke, 2 ke, 3 ke, dsbgnya, logic tu nanti implement dkt javascript.
 # memang functionality utk api JUST for fetch data. Any logic implement di client side.
