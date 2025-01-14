4e351d3c8636671982c8adb76b4a9bcbe52083c3
Haizi <haizukun@iziizz.com>
2025-01-14 10:07:56 +0800

    fix: restrict CORS to specific origin for enhanced security

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["*"],  # Allows all origins
+  allow_origins=["https://iziizz-tasker.x10.mx"],  # Allows all origins
   allow_credentials=True,
   allow_methods=["*"],  # Allows all methods
   allow_headers=["*"],  # Allows all headers
