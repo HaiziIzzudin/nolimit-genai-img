d8ec93f3a727c98c97daea7bbe89bba2a0103f87
haizi <79714350+HaiziIzzudin@users.noreply.github.com>
2025-01-14 10:31:43 +0800

    Update termux_user_install_pillow_this_script.sh

--- termux_user_install_pillow_this_script.sh
+++ termux_user_install_pillow_this_script.sh
@@ -1,3 +1,3 @@
 pip install wheel && \
 pkg install libjpeg-turbo libwebp && \
-LDFLAGS="-L/system/lib64/" CFLAGS="-I/data/data/com.termux/files/usr/include/" pip install Pillow
+LDFLAGS="-L/system/lib64/" CFLAGS="-I/data/data/com.termux/files/usr/include/" pip install --no-cache-dir --force-reinstall Pillow
